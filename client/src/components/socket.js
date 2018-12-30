c8d0b3b7e8dec37ce99557a24b85d9fe841885c9
Matt Outen <mattoutenphysed@gmail.com>
2018-12-30 10:20:05 -0600

    socket handle production paths

--- client/src/components/socket.js
+++ client/src/components/socket.js
@@ -1,5 +1,6 @@
 import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:5000');
+const keys = require('./socketIOpath');
+const socket = openSocket(keys.socketPath);
 function subscribeToTimer(cb) {
   socket.on('items', items => {
     cb(null, items);
