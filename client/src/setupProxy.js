a83ba4a8e86b3bcd71c82d312d53389846362e9a
matt.outen <matt.outen@mattouten.local>
2020-08-25 20:22:37 -0400

    fix proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,8 +2,8 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy("/api", { target: "https://localhost:5000", changeOrigin: true })
+    proxy("/api", { target: "http://localhost:5000", changeOrigin: true })
   );
 
-  app.use(proxy("/auth/google", { target: "https://localhost:5000" }));
+  app.use(proxy("/auth/google", { target: "http://localhost:5000" }));
 };
